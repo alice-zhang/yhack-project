5e31cf5775b4242680e02d26d2646a5656f037f4
Alice Zhang <ayz2105@columbia.edu>
2017-12-02 15:36:21 -0500

    alice's stuff

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_mongoengine import MongoEngine
 from mongoengine import StringField, EmailField
 
 from form import LoginForm
-#from form import EntryForm
 
 app = Flask(__name__)
 app.config['DEBUG'] = True #every time you run an error, it will show an error message; without this line, the server will crash and you won't know why
@@ -13,51 +12,66 @@ app.config['SECRET_KEY'] = 'SOME_STRING_THATS_SECRET'
 bootstrap = Bootstrap()
 bootstrap.init_app(app)
 
+app.config['MONGODB_SETTINGS'] = {
+    'db': 'yhack-db',
+    'host': 'ds125906.mlab.com',
+    'port': 25906,
+    'username': 'admin',
+    'password': 'gocolumbia2021'
+}
 
 db = MongoEngine()
 db.init_app(app)
 
-"""
-app.config['MONGODB_SETTINGS'] = {
-    'db': adi_academy,
-    'host': ds125126.mlab.com,
-    'port': 25126,
-    'username': ayz2105,
-    'password': #### !!!!!!!
-}
-"""
-"""
-class Entry(db.Document):
-	author = StringField(required=True) #new Entry has to have author defined
-	content = StringField()
-"""
+class User(db.Document):
+	name = StringField(required=True)
+	uni = StringField(required=True)
+	password = StringField(required=True)
+	email = EmailField(required=True)
+	phone = StringField(required=True)
+
+class Task(db.Document):
+	task = StringField(required=True)
+	descr = StringField(required=True)
+	reward = StringField(required=True)
 
 @app.route('/')
 def home():
-	return render_template('me.html', name='Alice', my_friends={'jonathan': 68, 'cesar': 62, 'nick': 100})
+	return render_template('home.html')
 
-"""
-@app.route('/diary', methods=['GET','POST'])
-def diary():
-	form = EntryForm() #create new instance
-	if form.validate_on_submit():
-		entry = Entry(author=form.author.data, concent=form.content.data)
-		entry.save()
-
-		redirect(url_for('diary'))
-	return render_template('/diary.html', form=form)
-"""
-
-@app.route('/login', methods = ['GET', 'POST'])
-def login(): #create decorator
-	username = None
+@app.route('/login', methods=['GET','POST'])
+def login():
 	form = LoginForm()
 	if form.validate_on_submit():
-		session['username'] = form.username.data
-		session['name'] = form.name.data
-		print session.get('username')
-		return redirect(url_for('login'))
-	return render_template('login.html', form=form, username=session.get('username'), name=session.get('name'))
+		user = User(name=form.name.data, uni=form.uni.data, password=form.password.data, email=form.email.data, phone=form.phone.data)
+		user.save()
+
+		session['name'] = user.name
+		session['uni'] = uni.name
+		session['password'] = user.password
+		session['email'] = user.email
+		session['phone'] = user.phone
+
+		redirect(url_for('login')) 
+	return render_template('login.html', form=form, name=session.get('name'), )
+
+@app.route('/asks', methods=['GET','POST'])
+def asks():
+	return render_template('asks.html')
+
+@app.route('/do', methods=['GET','POST'])
+	form = DoForm()
+	if form.validate_on_submit():
+		session['task'] = form.task.data
+		session['descr'] = form.descr.data
+		session['reward'] = form.reward.data
+
+		redirect(url_for('do'))
+	return render_template('do.html', form=form, task=session.get('task'), descr=session.get('descr'), reward=session.get('reward'))
+
+@app.route('/me', methods=['GET','POST'])
+def me():
+	return render_template('me.html')
 
 if __name__ == "__main__": #main is magic variable
 	app.run() 
--- form.py
+++ form.py
@@ -1,12 +1,36 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+#from wtforms_components import TimeField
 
 class LoginForm(FlaskForm):
-	email = StringField('Email')
 	name = StringField('Name')
-	username = StringField('Username')
+	uni = StringField('UNI')
 	password = PasswordField('Password')
-	submit = SubmitField('Register')
+	email = StringField('Email')
+	#phone = PhoneNumberField('Phone')
+	phone = StringField('Phone')
+	submit = SubmitField('Login')
+
+"""
+class DoForm(FlaskForm):
+	task = StringField('Task')
+	descr = StringField('Descr')
+	finish_time = TimeField('Finish Time')
+	location = StringField('Location')
+	reward = StringField('Reward')
+"""
+
+"""
+class SignupForm(FlaskForm):
+	name = StringField('Name')
+	uni = StringField('UNI')
+	password = PasswordField('Password')
+	email = StringField('Email')
+	#phone = PhoneNumberField('Phone')
+	phone = StringField('Phone')
+	submit = SubmitField('Sign Up')
+"""
+
 
 """
 class EntryForm(FlaskForm):
