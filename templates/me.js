6d11e4eb18f6ad2b57a9f34f7850ec1d848bf670
Irene Nam <yn2334@columbia.edu>
2017-12-02 17:20:31 -0500

    me page update

--- templates/me.js
+++ templates/me.js
@@ -1,4 +1,4 @@
-function openTask(list, typeoftask) {
+function openCity(evt, cityName) {
     // Declare all variables
     var i, tabcontent, tablinks;
 
@@ -15,6 +15,6 @@ function openTask(list, typeoftask) {
     }
 
     // Show the current tab, and add an "active" class to the button that opened the tab
-    document.getElementById(typeoftask).style.display = "block";
+    document.getElementById(cityName).style.display = "block";
     evt.currentTarget.className += " active";
 }
